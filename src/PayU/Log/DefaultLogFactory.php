6600d28dfbaceeb058e7034d79de4833bf51a8c8
Kenneth Madu Onah <kenneth@nanotech-solutions.co.za>
2023-05-10 00:51:00 +0200

    Addition of simple logger

--- src/PayU/Log/DefaultLogFactory.php
+++ src/PayU/Log/DefaultLogFactory.php
@@ -11,18 +11,13 @@ namespace PayU\Log;
 use Psr\Log\LoggerInterface;
 
 /**
- * Class PayUDefaultLogFactory
+ * Class DefaultLogFactory
  *
  * This factory is the default implementation of Log factory.
  *
  * @package PayU\Log
- * @copyright  Copyright (c) 2016 PayU
- * @license    http://opensource.org/licenses/LGPL-3.0  Open Software License (LGPL 3.0)
- * @link http://www.payu.co.za
- * @link http://help.payu.co.za/developers
- * @author Kenneth Onah <kenneth@netcraft-devops.com>
  */
-class PayUDefaultLogFactory
+class DefaultLogFactory
 {
     /**
      * Returns logger instance implementing LoggerInterface.
@@ -32,6 +27,6 @@ class PayUDefaultLogFactory
      */
     public function getLogger(string $className): LoggerInterface
     {
-        return new PayULogger($className);
+        return new Logger($className);
     }
 }
